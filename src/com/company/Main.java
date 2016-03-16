dce5cdf706e454946a4923c94d14280e0daef6be
HerpPotato <10009702@sbstudents.org>
2016-03-15 20:21:33 -0400

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -26,9 +26,9 @@ public class Main
         for(int i = 0; i < 1000; i++)
         {
             long start = System.currentTimeMillis();
-            Trainer kimJongIl = new Trainer(2, 1, 2, new int[]{100, 100, 10, 10, 2}, new double[][]{{2, 3}, {4, 5}, {4, 2}}, new double[][]{{6}, {20}, {8}});
-            kimJongIl.setSeed(i);
-            kimJongIl.teachMult(2, start);
+            Trainer trainer = new Trainer(2, 1, 2, new int[]{100, 100, 10, 10, 2}, new double[][]{{2, 3}, {4, 5}, {4, 2}}, new double[][]{{6}, {20}, {8}});
+            trainer.setSeed(i);
+            trainer.teachMult(2, start);
             time[i] = (double) (System.currentTimeMillis() - start) / 1000;
             if (time[i] < 0.8)
             {
@@ -46,9 +46,9 @@ public class Main
         for(int i = 0; i < niceTimes; i++)
         {
             long start = System.currentTimeMillis();
-            Trainer kimJongIl = new Trainer(2, 1, 2, new int[]{100, 100, 10, 10, 2}, new double[][]{{2, 3}, {4, 5}, {4, 2}}, new double[][]{{6}, {20}, {8}});
-            kimJongIl.setSeed(niceSeed[i]);
-            kimJongIl.teachMult(3, start);
+            Trainer trainer = new Trainer(2, 1, 2, new int[]{100, 100, 10, 10, 2}, new double[][]{{2, 3}, {4, 5}, {4, 2}}, new double[][]{{6}, {20}, {8}});
+            trainer.setSeed(niceSeed[i]);
+            trainer.teachMult(3, start);
             niceTime[i] = (double) (System.currentTimeMillis() - start) / 1000;
             totalTime += niceTime[i];
         }
@@ -76,9 +76,9 @@ public class Main
         for(int i = 0; i < greatTimes; i++)
         {
             long start = System.currentTimeMillis();
-            Trainer kimJongIl = new Trainer(2, 1, 2, new int[]{100, 100, 10, 10, 2}, new double[][]{{2, 3}, {4, 5}, {4, 2}}, new double[][]{{6}, {20}, {8}});
-            kimJongIl.setSeed(greatSeed[i]);
-            kimJongIl.teachMult(4, start);
+            Trainer trainer = new Trainer(2, 1, 2, new int[]{100, 100, 10, 10, 2}, new double[][]{{2, 3}, {4, 5}, {4, 2}}, new double[][]{{6}, {20}, {8}});
+            trainer.setSeed(greatSeed[i]);
+            trainer.teachMult(4, start);
             greatTime[i] = (double) (System.currentTimeMillis() - start) / 1000;
             totalTime += greatTime[i];
         }
@@ -108,9 +108,9 @@ public class Main
         for(int i = 0; i < excellentTimes; i++)
         {
             long start = System.currentTimeMillis();
-            Trainer kimJongIl = new Trainer(2, 1, 2, new int[]{100, 100, 10, 10, 2}, new double[][]{{2, 3}, {4, 5}, {4, 2}}, new double[][]{{6}, {20}, {8}});
-            kimJongIl.setSeed(excellentSeed[i]);
-            kimJongIl.teachMult(5, start);
+            Trainer trainer = new Trainer(2, 1, 2, new int[]{100, 100, 10, 10, 2}, new double[][]{{2, 3}, {4, 5}, {4, 2}}, new double[][]{{6}, {20}, {8}});
+            trainer.setSeed(excellentSeed[i]);
+            trainer.teachMult(5, start);
             excellentTime[i] = (double) (System.currentTimeMillis() - start) / 1000;
             totalTime += excellentTime[i];
         }
