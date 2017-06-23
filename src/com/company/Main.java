2320068c86c3ca2a045794bb7ecb8e08a514f4d5
HerpPotato <adam.a1d9c@gmail.com>
2017-06-23 13:31:22 -0400

    reverted to working version

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,14 +2,12 @@ package com.company;
 
 public class Main
 {
-    //for efficiency, the neural network has been formatted to run optimally with only one output
-    //as of now, multiple output backprop has not been fully implemented
     public static void main(String[] args)
     {
-        double[][] questions = new double[][]{{0, 1, 0}, {4, 0, 0}, {3, 1, 2}, {5, 0, 0}, {0, 1, 0}, {1, 1, 0}, {2, 5, 0}};
-        double[][] answers = new double[][]{{13}, {15}, {17}, {18}, {19}, {20}, {21}};
-        int[] layerInformation = new int[]{5, 3, 2};
-        Trainer trainer = new Trainer(3, 1, 3, layerInformation, questions, answers);
+        Trainer trainer = new Trainer(3, 1, 1, new int[]{2}, new double[][]{{0, 0, 1}, {0, 1, 1}, {1, 0, 1}, {1, 1, 1}}, new double[][]{{0}, {0}, {0}, {1}});
         trainer.setSeed(0);
+        long start = System.currentTimeMillis();
+        trainer.teachAllTillLearned(10);
+        System.out.println(System.currentTimeMillis() - start);
     }
 }
